c8474647c4c28ead1834662fca1c3b9121f2dec5
ArtemIlliushchenko <artemillushchenko30@gmail.com>
2026-03-03 15:09:50 +0000

    Deploying to gh-pages from @ ArtemIlliushchenko/goit-js-hw-10@945be86b535c2d3ef9597ad50759003cb592d431 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{i}from"./assets/vendor-BbbuE1sJ.js";const m=document.querySelector(".form");m.addEventListener("submit",function(t){t.preventDefault();const s=parseInt(t.target.delay.value),o=t.target.state.value;new Promise((e,r)=>{setTimeout(()=>{o==="fulfilled"?e(s):r(s)},s)}).then(e=>{i.success({title:"✅",message:`Fulfilled promise in ${e}ms`,position:"topRight"})}).catch(e=>{i.error({title:"❌",message:`Rejected promise in ${e}ms`,position:"topRight"})})});
+//# sourceMappingURL=2-snackbar.js.map
